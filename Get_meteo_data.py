47e0708519f8e3479043446c8ac0f3fbfe67b85a
GabrieleGhisleni <74197369+GabrieleGhisleni@users.noreply.github.com>
2021-04-20 00:46:48 +0200

    Initial commit.  - Creato il file per il fetching dei dati meteo aggregati a livello regionale (per capoluogo di regione).  - Salvati in un file JSON tutte le variabili.  - Aggiunto alle variabili timestamp sistematizzata.  - Aggiunto alle variabili la regione d'italia.

---
+++ Get_meteo_data.py
@@ -0,0 +1,83 @@
+import requests, time, datetime, json
+from pprint import pprint
+from typing import List
+
+
+class GetMeteoData():
+    def dict_regions(self) -> dict:
+        " Returns a dictionary having as key the regions divided by Terna\
+        and having as values all the capoluoghi of that region.           "
+        nord = ['Aosta','Genova','Torino','Milano','Trento','Venezia','Bologna','Trieste']
+        centro_nord = ['Perugia','Firenze','Ancona']
+        centro_sud = ['Aquila','Roma','Aquila','Roma','Campobasso']
+        sud = ['Napoli','Bari','Potenza']
+        sicilia = ['Palermo']
+        sardegna = ['Sardegna']
+        calabria = ['Catanzaro']
+        return dict(nord=nord, centro_nord=centro_nord, centro_sud=centro_sud,
+                    sud=sud, sicilia=sicilia,sardegna=sardegna, calabria=calabria)
+
+    def organize_date(self) -> str:
+        "since AWS works with different timezone (-2 hours) it returns the data\
+        already as string with the same format as Terna Data"
+        return (datetime.datetime.now()+datetime.timedelta(hours=2)).strftime("%d/%m/%Y %H:%M:%S %p")
+
+    def fetching_current_meteo_json(self)-> List[dict]:
+        "fetch all the meteo data from the regions, return a list of dict aka json"
+        res = []
+        regions = self.dict_regions()
+        for regione in regions:
+            for capoluogo in regions[regione]:
+                try:
+                    url = " https://api.openweathermap.org/data/2.5/weather?appid=a054032d5e094190a9eba85b70421ff3&units=metric&q={}".format(capoluogo)
+                    response = requests.request("GET", url)
+                    if not response.ok:
+                        print ("Something wrong with the respunsus ok API" + str(response) + "at " + capoluogo)
+                        pass
+                    else:
+                        tmp = response.json()
+                        tmp["region"] = regione
+                        tmp["organized_data"] = self.organize_date()
+                        res.append(tmp)
+                except Exception: #done so the program won't crash if something go wrong.
+                    print(" --> Fatal error with the requests connection <-- ")
+        return res
+
+
+class JsonManagerMeteo():
+    def load(self)->List[dict]:
+        try:
+            with open("storico_meteo.json", "r") as file:
+                storico = json.load(file)
+                file.close()
+            return storico
+        except Exception:
+            with open("storico_meteo.json", "w") as file:
+                print("File not found, created 'storico_meteo.json'.")
+                self.first_load()
+                return self.load()
+
+    def first_load(self):
+        print("First Load.")
+        obs = (GetMeteoData().fetching_current_meteo_json())
+        with open("storico_meteo.json", "w") as file:
+            json.dump(obs, file, indent=4)
+
+    def update(self)->None:
+        storico = self.load()
+        new_obs = (GetMeteoData().fetching_current_meteo_json())
+        update = storico + new_obs # must optimize this process
+        with open("storico_meteo.json", "w") as file:
+            json.dump(update, file, indent=4)
+
+
+if __name__ == "__main__":
+    while True:
+        if str(datetime.datetime.now().time().minute).endswith('0')\
+            or str(datetime.datetime.now().time().minute).endswith('5'):
+                manager = JsonManagerMeteo()
+                manager.update()
+                time.sleep(290)
+
+
+
