ddb86c83f0a1d9346828c2f25eaaf250aef67708
Gabriel Ghisleni <gabriele.ghisleni01@gmail.com>
2021-07-13 12:15:33 +0200

    - Changed InfoGraphic

--- Display/views.py
+++ Display/views.py
@@ -23,40 +23,36 @@ def make_plot(dates, load, thermal, wind, hydro, photovoltaic, geothermal, bioma
     fig.add_trace(go.Scatter(name="Biomass", x=dates, y=biomass, fill='tonexty', stackgroup='one', hoverinfo='x+y'))
     fig.add_trace(go.Scatter(name="Geothermal", x=dates, y=geothermal, fill='tonexty', stackgroup='one', hoverinfo='x+y'))
     x_ticks = [date.split(" ")[1][:5] for date in dates]
-
-    # title = f"""<b>Energy Predictions for {dates[0].split(" ")[0]}</b>"""
-    # title_dict = dict(size=35, family='Bold-Courier', color='black')
-    legend_dict = dict(orientation="h", yanchor="middle", y=1.01, xanchor="right", x=1.01)
-    xaxis_dict = dict(tickmode='array', tickvals=dates, ticktext=x_ticks, tick0=0.2, tickangle=30, dtick=0.5)
-    yaxis_dict = dict(title="Generation/consumption in  GW/H", range=[0, 65], showgrid=True)
-    margin_dict = dict(l=75, r=50, b=50, t=35, pad=7.5)
+    tickfont = dict(family='Old Standard TT, serif',size=18, color='black' )
+    legend_dict = dict(orientation="h", yanchor="middle", y=1.01, xanchor="right", x=1.01, font = dict(size=20))
+    xaxis_dict = dict(tickmode='array', tickvals=dates, ticktext=x_ticks, tick0=0.2, tickangle=30, dtick=0.5, tickfont=tickfont)
+    yaxis_dict = dict(title=dict(text="Generation/consumption in  GW/H",font_size=25),
+                      range=[0, 65], showgrid=True, tickfont=tickfont)
+    margin_dict = dict(l=100, r=50, b=50, t=35, pad=7.5)
 
     fig.update_layout(legend=legend_dict,
                       xaxis=xaxis_dict, yaxis=yaxis_dict, margin=margin_dict, width=1400, height=600,
-                      template="gridon", paper_bgcolor='rgb(245,245,245)') # ,title=title, title_font=title_dict,
+                      template="gridon", paper_bgcolor='rgb(240,248,255)')
 
     load = plot({'data': fig}, output_type="div", include_plotlyjs=False, show_link=False, link_text="")
     return load
 
 def make_empty_plot():
     fig = go.Figure()
-    fig.add_annotation(x=-1, y=25, text="No Data to Display Yet",
-                       font=dict(family="sans serif", size=35, color="crimson"),
-                       showarrow=False, yshift=10)
+    fig.add_annotation(x=-1, y=0, text="No Data To Display Yet",
+                       font=dict(family="sans serif, bold", size=45, color="crimson"),
+                       showarrow=False, yshift=0)
     x, y = [-1], [0]
     for source in ['Load', 'Thermal', 'Photovoltaic', 'Hydro', 'Wind', 'Biomass', 'Geothermal']:
         fig.add_trace(go.Scatter(name=source, x=x, y=y, mode='lines'))
-    title = f"""<b>Load ~ Renewable productions and Thermal</b>"""
-    title_dict = dict(size=18, family='Bold-Courier', color='crimson')
-    legend_dict = dict(orientation="h", yanchor="middle", y=1.01, xanchor="right", x=1.01)
-    xaxis_dict = dict(title="Hours", tickmode='array', tick0=0.2, tickangle=30, dtick=0.5, showgrid=False)
-    yaxis_dict = dict(title="Generation/consumption in  GW/H", range=[0, 60], showgrid=False)
-    margin_dict = dict(l=75, r=50, b=75, t=100, pad=7.5)
-    fig.update_layout(title=title, title_font=title_dict, legend=legend_dict,
-                      xaxis=xaxis_dict, yaxis=yaxis_dict, margin=margin_dict, width=800, height=600,
-                      template="gridon", paper_bgcolor='rgb(245,245,245)')
-    load = plot({'data': fig}, output_type="div", include_plotlyjs=False, show_link=False, link_text="")
-    return load
+
+    xaxis_dict = dict(visible=False, showgrid=False)
+    yaxis_dict = dict(visible=False, showgrid=False)
+    margin_dict = dict(l=40, r=40, b=40, t=40, pad=7.5)
+    fig.update_layout(xaxis=xaxis_dict, yaxis=yaxis_dict, margin=margin_dict, showlegend=False,
+                      width=900, height=600, template="gridon", paper_bgcolor='rgba(255,10,10,4)')
+    empty = plot({'data': fig}, output_type="div", include_plotlyjs=False, show_link=False, link_text="")
+    return empty
 
 def difference(load, thermal, wind, hydro, photovoltaic, geothermal, biomass):
     summation = np.array(wind) + np.array(thermal) + np.array(hydro) + np.array(photovoltaic) + np.array(geothermal) + np.array(biomass)
@@ -73,11 +69,81 @@ def difference(load, thermal, wind, hydro, photovoltaic, geothermal, biomass):
         res.append(tmp)
     return res
 
-
 def get_data(day='today'):
     redis = dbs.RedisDB()
     data = redis.get_data(day=day)
     return data
+
+def unroll_df(df, cat, groups, dates):
+    if not df.empty:
+        df['month'] = df[dates].dt.strftime("%B")
+        df['hour'] = df[dates].dt.strftime("%H").astype('int')
+        storage = []
+        for unique in np.unique(df[cat]):
+            temp = df.loc[df[cat] == unique, :].groupby(groups).mean().reset_index()
+            temp[cat] = unique
+            storage.append(temp)
+        return pd.concat(storage)
+    else: return pd.DataFrame()
+
+def get_plots_data():
+    db = dbs.MySqlDB()
+    query_energy = "select energy_source as src, date, generation as y from energy_generation"
+    query_load = "SELECT total_load as y, holiday, date from energy.energy_load"
+    energy = db.query_from_sql_to_pandas(query_energy)
+    load = db.query_from_sql_to_pandas(query_load)
+    return energy,load
+
+def make_energy_panel_plot(energy_df, load_df):
+    titles = ['Load', 'Thermal & Hydro', 'Others Renewable']
+    x_m, y_m = 'Hours of the day', 'Load - Generation in [GWH]'
+    fig = make_subplots(rows=1, cols=3, print_grid=False, subplot_titles=titles, x_title=x_m, y_title=y_m, shared_yaxes=True)
+    lined = dict(width=4)
+    for src in np.unique(energy_df.src):
+        if src == 'Hydro' or src == 'Thermal':
+            fig.add_trace(go.Scatter(x=energy_df.loc[energy_df.src == src].hour, y=energy_df.loc[energy_df.src == src].y,
+                           name=src, mode='lines', showlegend=False, line=lined), row=1, col=2)
+        else:
+            fig.add_trace(go.Scatter(x=energy_df.loc[energy_df.src == src].hour, y=energy_df.loc[energy_df.src == src].y,
+                           name=src, mode='lines',showlegend=True,line=lined), row=1, col=3)
+
+    fig.add_trace(go.Scatter(x=load_df.groupby('hour').mean().reset_index().hour, name='load',showlegend=False,
+                             y=load_df.groupby('hour').mean().reset_index().y, mode='lines',line=lined), row=1, col=1)
+
+    margin_dict = dict(l=100, r=75, b=75, t=60, pad=7.5)
+    yaxis_dict = dict(zeroline=False, tickvals=[i for i in range(0, 45, 2)], ticktext=[i for i in range(0, 45, 2)])
+    xaxis_dict = dict(tickvals=[i for i in range(0, 24)], tickmode='array', tick0=0.2, tickangle=90, zeroline=False,
+                      dtick=0.5, showgrid=False, ticktext=[f"0{str(i)}" if i < 10 else str(i) for i in range(0, 24)])
+    for i in range(1, 4): fig.update_xaxes(xaxis_dict, row=1, col=i)
+    for i in range(1, 4): fig.update_yaxes(yaxis_dict, row=1, col=i)
+    legend_dict = dict(yanchor="middle", y=0.89, xanchor="right", x=1.0, font = dict(size=20))
+    fig.update_layout(height=700, width=1400, template='gridon', paper_bgcolor='rgb(240,248,255)',
+                      margin=margin_dict, legend=legend_dict)
+    fig.update_annotations(font_size=30, font_family='italic')
+    return plot({'data': fig}, output_type="div", include_plotlyjs=False, show_link=False, link_text="")
+
+def make_load_panel_plot(load_df):
+    titles = ['Load on Holiday', 'Load on Sunday', 'Load on Saturday', 'Load during Weeks']
+    x_m, y_m = 'Hours of the day', 'Load - in [GWH]'
+    lined = dict(width=4)
+    fig = make_subplots(rows=1, cols=4, print_grid=False, subplot_titles=titles, x_title=x_m, y_title=y_m, shared_yaxes=True)
+    fig.add_trace(go.Scatter(x=load_df.loc[load_df.holiday == 'holiday'].hour,
+                             y=load_df.loc[load_df.holiday == 'holiday'].y, name='Holiday', mode='lines', line=lined), row=1, col=1)
+    fig.add_trace(go.Scatter(x=load_df.loc[load_df.holiday == 'sunday'].hour,
+                             y=load_df.loc[load_df.holiday == 'sunday'].y, name='Sunday', mode='lines', line=lined), row=1, col=2)
+    fig.add_trace(go.Scatter(x=load_df.loc[load_df.holiday == 'saturday'].hour,
+                             y=load_df.loc[load_df.holiday == 'saturday'].y, name='Saturday', mode='lines', line=lined), row=1, col=3)
+    fig.add_trace(go.Scatter(x=load_df.loc[load_df.holiday == 'no'].hour,
+                             y=load_df.loc[load_df.holiday == 'no'].y, name='Weeks', mode='lines', line=lined), row=1, col=4)
+    margin_dict = dict(l=100, r=75, b=75, t=60, pad=7.5)
+    xaxis_dict = dict(tickvals=[i for i in range(0, 24)], tickmode='array', tick0=0.2, tickangle=90, zeroline=False,
+                      dtick=0.5, showgrid=False, ticktext=[f"0{str(i)}" if i < 10 else str(i) for i in range(0, 24)])
+    for i in range(1, 5): fig.update_xaxes(xaxis_dict, row=1, col=i)
+    for i in range(1, 5): fig.update_yaxes(dict(zeroline=False), row=1, col=i)
+    fig.update_layout(height=600, width=1500, template='gridon', paper_bgcolor='rgb(240,248,255)', showlegend=False, margin=margin_dict)
+    fig.update_annotations(font_size=30, font_family='italic')
+    return plot({'data': fig}, output_type="div", include_plotlyjs=False, show_link=False, link_text="")
+
 
 def today_pred(requests):
     data = get_data('today')
@@ -100,6 +166,30 @@ def tomorrow_pred(requests):
         tomorrow_s = (dt.datetime.now()+dt.timedelta(days=1)).strftime("%B, %d-%Y")
         context = dict(plot_div=fig, day='Tomorrow', probability=differences, day_s=tomorrow_s)
         return render(requests, "Display/prediction.html", context)
+
+def home(requests):
+    return render(requests, 'Display/home.html', {'today-prediction': "HOME"})
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created, from now on you will receive an email each time the prediction are update!")
+            return redirect("today")
+    else:  form = UserRegistrationForm()
+    return render(request, 'Display/newsletter.html', {"form": form})
+
+def infographic(requests):
+    energy, load = get_plots_data()
+    energies = unroll_df(energy, cat='src', groups='hour', dates='date')
+    loads = unroll_df(load, cat='holiday', groups='hour', dates='date')
+    if not energies.empty and not loads.empty: first_panel = make_energy_panel_plot(energies, loads)
+    else: first_panel = make_empty_plot()
+    if not loads.empty: second_panel= make_load_panel_plot(loads)
+    else: second_panel = make_empty_plot()
+    context = dict(plot_div=first_panel, plot_load=second_panel)
+    return render(requests, "Display/infos.html", context)
 
 
 @api_view(['GET'])
@@ -184,117 +274,6 @@ def Energy_Full_Rest_API(requests):
     df.sort_values(['date'], inplace=True)
     return Response(df.to_dict(), status=status.HTTP_200_OK)
 
-def home(requests):
-    return render(requests, 'Display/home.html', {'today-prediction': "HOME"})
-
-def about(requests):
-    return render(requests, 'Display/about.html', {'today-prediction': "about"})
-
-def register(request):
-    if request.method == "POST":
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Account created, from now on you will receive an email each time the prediction are update!")
-            return redirect("today")
-    else:  form = UserRegistrationForm()
-    return render(request, 'Display/newsletter.html', {"form": form})
 
 
-def renewable_plot(db):
-    df = db.query_from_sql_to_pandas(query="select energy_source,date,generation from energy_generation")
-    df['month'] = df.date.dt.strftime("%B")
-    df['hour'] = df.date.dt.strftime("%H").astype('int')
-    res = []
-    for energy in np.unique(df.energy_source):
-        tmp = df.loc[df.energy_source == energy, :].groupby('hour').mean().reset_index()
-        tmp['energy'] = energy
-        res.append(tmp)
-    if res:
-        res = pd.concat(res)
-        fig = make_subplots(rows=1, cols=2, print_grid=False,x_title='Hours of the day',
-                    y_title='Generation in GWH', subplot_titles=['Thermal and Hydro<br>', 'Others Renewable Energies<br>'], shared_yaxes=True)
-        for energy in np.unique(res.energy):
-            if energy == 'Hydro' or energy == 'Thermal':
-                fig.add_trace(go.Scatter(x=res.loc[res.energy == energy].hour, y=res.loc[res.energy == energy].generation,
-                                         name=energy, mode='lines'), row=1, col=1)
-            else: fig.add_trace(go.Scatter(x=res.loc[res.energy == energy].hour, y=res.loc[res.energy == energy].generation,
-                                           name=energy, mode='lines'),row=1, col=2)
-        margin_dict = dict(l=75, r=75, b=75, t=50, pad=10)
-        legend_dict = dict(orientation="v", yanchor="middle", y=0.85, xanchor="right", x=1)
-        fig.update_layout(height=600, width=1100, template='gridon', paper_bgcolor='rgb(245,245,245)',
-                          margin=margin_dict, legend=legend_dict)
-        fig.update_annotations(font_size=20, font_family='italic')
-        return plot({'data': fig}, output_type="div", include_plotlyjs=False, show_link=False, link_text="")
-    else: return make_empty_plot()
 
-
-def load_plot(db):
-    df = db.get_training_load_data(whole=True)
-    df.rename(columns={'str_month':'month'}, inplace=True)
-    summer = ['june', 'july', 'august']
-    winter = ['decembter', 'january', 'february']
-    df_summer = df.loc[df.month.isin(summer), :]
-    df_winter = df.loc[df.month.isin(winter), :]
-    if not df_winter.empty:
-        tmp_winter = []
-        for h in np.unique(df_winter.holiday):
-            m = df_winter.loc[df_winter.holiday == h, :].groupby('str_hour').mean().reset_index()
-            m['holiday'] = h
-            tmp_winter.append(m)
-        final_winter = pd.concat(tmp_winter)
-        final_winter.str_hour = final_winter.str_hour.astype('int')
-        final_winter.sort_values('str_hour', inplace=True)
-        tmp_summer = []
-        for h in np.unique(df_summer.holiday):
-            m = df_summer.loc[df_summer.holiday == h, :].groupby('str_hour').mean().reset_index()
-            m['holiday'] = h
-            tmp_summer.append(m)
-        final_summer = pd.concat(tmp_summer)
-        final_summer.str_hour = final_summer.str_hour.astype('int')
-        final_summer.sort_values('str_hour', inplace=True)
-        titles = ['Winter weeks', 'Winter Saturday', 'Winter Sunday', 'Winter Holiday',
-                  'Summer weeks', 'Summer Saturday', 'Summer Sunday', 'Summer Holiday']
-        fig = make_subplots(rows=2, cols=4, print_grid=False, x_title='Hours of the day',
-                    y_title='Load in GWH', subplot_titles=titles, shared_yaxes=True, vertical_spacing=0.1)
-        for h in np.unique(final_winter.holiday):
-            if h == 'no':
-                fig.add_trace(go.Scatter(x=final_winter.loc[final_winter.holiday == h].str_hour,
-                                         y=final_winter.loc[final_winter.holiday == h].total_load, mode='lines'), row=1, col=1)
-            elif h == 'saturday':
-                fig.add_trace(go.Scatter(x=final_winter.loc[final_winter.holiday == h].str_hour,
-                                         y=final_winter.loc[final_winter.holiday == h].total_load, mode='lines'), row=1, col=2)
-            elif h == 'sunday':
-                fig.add_trace(go.Scatter(x=final_winter.loc[final_winter.holiday == h].str_hour,
-                                         y=final_winter.loc[final_winter.holiday == h].total_load, mode='lines'), row=1, col=3)
-            elif h == 'holiday':
-                fig.add_trace(go.Scatter(x=final_winter.loc[final_winter.holiday == h].str_hour,
-                                         y=final_winter.loc[final_winter.holiday == h].total_load, mode='lines'), row=1, col=4)
-
-        for h in np.unique(final_summer.holiday):
-            if h == 'no':
-                fig.add_trace(go.Scatter(x=final_summer.loc[final_summer.holiday == h].str_hour,
-                                         y=final_summer.loc[final_summer.holiday == h].total_load, mode='lines'), row=2, col=1)
-            elif h == 'saturday':
-                fig.add_trace(go.Scatter(x=final_summer.loc[final_summer.holiday == h].str_hour,
-                                         y=final_summer.loc[final_summer.holiday == h].total_load, mode='lines'), row=2, col=2)
-            elif h == 'sunday':
-                fig.add_trace(go.Scatter(x=final_summer.loc[final_summer.holiday == h].str_hour,
-                                         y=final_summer.loc[final_summer.holiday == h].total_load, mode='lines'), row=2, col=3)
-            elif h == 'holiday':
-                fig.add_trace(go.Scatter(x=final_summer.loc[final_summer.holiday == h].str_hour,
-                                         y=final_summer.loc[final_summer.holiday == h].total_load, mode='lines'), row=2, col=4)
-
-        margin_dict = dict(l=75, r=75, b=75, t=75, pad=10)
-        fig.update_layout(height=1000, width=1400, template='gridon', paper_bgcolor='rgb(245,245,245)',
-                          margin=margin_dict, showlegend=False)
-        fig.update_annotations(font_size=20, font_family='italic')
-        return plot({'data': fig}, output_type="div", include_plotlyjs=False, show_link=False, link_text="")
-    else: return make_empty_plot()
-
-def infographic(requests):
-    db = dbs.MySqlModels()
-    plot_renewable = renewable_plot(db)
-    plot_load = load_plot(db)
-    context = dict(plot_div=plot_renewable, plot_load=plot_load)
-    return render(requests, "Display/infos.html", context)
